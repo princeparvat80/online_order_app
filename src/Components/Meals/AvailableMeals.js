72b23cd0464f92f502eb3d971d21e49688c7ee52
Prince Parwat <princeparvat80@gmail.com>
2022-08-03 01:48:08 +0530

    Menu Iteam Created

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -2,10 +2,18 @@ import React from "react";
 import Card from "../UI/Card";
 import classes from "../Meals/Meals_Css/AvailableMeals.module.css";
 import * as DUMMY_DATA from "../../SharedModules/DummyData.js";
+import MealItem from "../Meal_Items/MealItem";
 
 export default function AvailableMeals(props) {
   const Data = DUMMY_DATA.DUMMY_MEALS;
-  const mealList = Data.map((meal) => <list>{meal.name}</list>);
+  const mealList = Data.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
