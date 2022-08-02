3752d4b869d634ac8dced3da3a120698f3cbde75
Prince Parwat <princeparvat80@gmail.com>
2022-08-03 00:13:10 +0530

    Header Created

---
+++ src/SharedModules/Layout/HeaderCartButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../../Components/Cart/CartIcon";
+export default function HeaderCartButton(props) {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>5</span>
+    </button>
+  );
+}
